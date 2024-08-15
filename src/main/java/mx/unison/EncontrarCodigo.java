05780d8b87e79595c81004dd9d6262b8228e0142
EddyStampede22 <a217219286@unison.mx>
2024-08-15 12:29:51 -0700

    Update EncontrarCodigo.java

--- src/main/java/mx/unison/EncontrarCodigo.java
+++ src/main/java/mx/unison/EncontrarCodigo.java
@@ -1,1 +1,26 @@
+package mx.unison;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EncontrarCodigo {
+    public static void main(String[] args) throws IOException {
+        FileReader archivoFisico = new FileReader("C:/Users/marti/IdeaProjects/codigospostales/src/main/java/mx/unison/codigos_postales.csv");
+        BufferedReader archivoLogico = new BufferedReader(archivoFisico);
+        String registro = null;
+        String campos[];
+        for(String n : args){
+            while ( (registro = archivoLogico.readLine()) != null ) {
+                campos = registro.split(",");
+                if(n.equalsIgnoreCase(campos[0])){
+                    System.out.println(campos[2]);
+                    break;
+                }
+
+            }
+        }
+
+
+    }
+}
